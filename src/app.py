827f6ed87e16752381a705193cfbe8b42fa9b151
sherry-rui-shan <66582295+sherry-rui-shan@users.noreply.github.com>
2026-02-22 00:19:24 +0900

    Fix formatting of API description in app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 """
-High School Management System API
+High School Management System API 
 
 A super simple FastAPI application that allows students to view and sign up
 for extracurricular activities at Mergington High School.
